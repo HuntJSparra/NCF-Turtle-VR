c909d51573cd187e632e900454d565de302b106b
NCFVRLab <55799153+NCFVRLab@users.noreply.github.com>
2019-10-25 18:01:50 -0400

    added back grabbing and started adding laser pointers

---
+++ VR Research/Assets/Scripts/LaserPointerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointerControl : MonoBehaviour
+{
+    public LineRenderer Left, Right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger) == true && OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger) == false)
+        {
+            Left.enabled = true;
+        }
+        else
+        {
+            Left.enabled = false;
+        }
+        if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger) == true && OVRInput.Get(OVRInput.Touch.SecondaryIndexTrigger) == false)
+        {
+            Right.enabled = true;
+        }
+        else
+        {
+            Right.enabled = false;
+        }
+    }
+}
