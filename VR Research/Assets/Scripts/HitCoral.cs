4cd710ac0ccac32d074400a52d8ca38ff52fabe8
NCFVRLab <55799153+NCFVRLab@users.noreply.github.com>
2019-12-07 20:29:28 -0500

    added small check to trash collision

--- VR Research/Assets/Scripts/HitCoral.cs
+++ VR Research/Assets/Scripts/HitCoral.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class HitCoral : MonoBehaviour
 {
+    Dictionary<Collider, int> hitPrior;
+    private void Start()
+    {
+        hitPrior = new Dictionary<Collider, int>();
+    }
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Trash" || other.gameObject.tag == "Soda" || other.gameObject.tag == "Water")
         {
-            UpdatePoints.removePoint();
+            if (!hitPrior.ContainsKey(other))
+            {
+                UpdatePoints.removePoint();
+                hitPrior.Add(other, 60);
+            }
+        }
+    }
+    private void Update()
+    {
+        foreach (Collider obj in hitPrior.Keys)
+        {
+            hitPrior[obj] = hitPrior[obj] - 1;
+            if(hitPrior[obj] == 0)
+            {
+                hitPrior.Remove(obj);
+            }
         }
     }
 }
