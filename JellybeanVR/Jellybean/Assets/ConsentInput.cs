5c4f7cecdb09d9540e5cb821df2de3724fd61210
Hunt J. Sparra <>
2021-05-17 20:31:41 -0400

    Added consent scene for data collection

---
+++ JellybeanVR/Jellybean/Assets/ConsentInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsentInput : MonoBehaviour
+{
+    private bool transitioning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Google.XR.Cardboard.Api.IsTriggerPressed || Input.GetMouseButtonDown(0))
+        {
+            if (transitioning == false)
+            {
+                SceneTransition.instance.ChangeScene("Menu");
+                transitioning = true;
+            }
+        }
+    }
+}
